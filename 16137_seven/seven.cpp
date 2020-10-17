67b1408a4b2aabe249fec40be406fd65af693bea
hogaengMac <jeff8080@naver.com>
2020-10-18 02:41:07 +0900

    seven has new method, but not thunder

--- 16137_seven/seven.cpp
+++ 16137_seven/seven.cpp
@@ -10,87 +10,130 @@ int n;
 int m;
 int b[10][10]={0,};
 int v[10][10]={0,};
-int visited[10][10]={0,};
 int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
 vector<pair<int,int> > rights;
 vector<pair<int,int> > downs;
 vector<int> track;
 void bfs(){
-    vector<pair<int,int> > que;
-    que.push_back(make_pair(0,0));
-    v[0][0]=1;
-	visited[0][0]=1;
-    while(que.size()>0){
-		int x=que[0].first,y=que[0].second,t=v[x][y];
-#ifdef debug
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++)
-				cout<<visited[i][j]<<' ';
-			cout<<endl;
+	for(int i=1;i<n;i++){
+		if(b[0][i]==1)
+			v[0][i]=v[0][i-1]+1;
+		else if(b[0][i]>0){
+			if(i+1<n && b[0][i+1]==1){
+				int cnt=1;
+				while((v[0][i-1]+cnt)%b[0][i]>0)cnt++;
+				v[0][i]=v[0][i-1]+cnt;
+				v[0][i+1]=v[0][i]+1;
+				i++;
+			}
 		}
-		cout<<endl;
+		else{
+			auto iter=find(rights.begin(),rights.end(),make_pair(0,i));
+			if(iter!=rights.end()){
+				int cnt=1;
+				while((v[0][i-1]+cnt)%m>0)cnt++;
+				v[0][i]=v[0][i-1]+cnt;
+				if(i+1<n){
+					v[0][i+1]=v[0][i]+1;
+					i++;
+				}
+			}
+		}
+	}
+	for(int i=1;i<n;i++){
+		if(b[i][0]==1)
+			v[i][0]=v[i-1][0]+1;
+		else if(b[i][0]>0){
+			if(i+1<n && b[i+1][0]==1){
+				int cnt=1;
+				while((v[i-1][0]+cnt)%b[i][0]>0)cnt++;
+				v[i][0]=v[i-1][0]+cnt;
+				v[i+1][0]=v[i][0]+1;
+				i++;
+			}
+		}
+		else{
+			auto iter=find(downs.begin(),downs.end(),make_pair(i,0));
+			if(iter!=downs.end()){
+				int cnt=1;
+				while((v[i-1][0]+cnt)%m>0)cnt++;
+				v[i][0]=v[i-1][0]+cnt;
+				if(i+1<n){
+					v[i+1][0]=v[i][0]+1;
+					i++;
+				}
+			}
+		}
+	}
+	for(int i=1;i<n;i++)
+		for(int j=1;j<n;j++){
+			int bi=i-1,bj=j-1;
+
+			int upv=INT_MAX;
+			if(b[i][j]==1){
+				if(b[bi][j]==1)
+					upv=v[bi][j]+1;
+				else{
+					if(b[bi][j]==0){
+						auto iter=find(downs.begin(),downs.end(),make_pair(bi,j));
+						if(iter!=downs.end())
+							upv=v[bi][j]+1;
+					}
+					else
+						upv=v[bi][j]+1;
+				}
+			}
+			else if(b[i][j]==0){
+				auto iter=find(downs.begin(),downs.end(),make_pair(i,j));
+				if(iter!=downs.end()){
+					int cnt=1;
+					while((v[bi][j]+cnt)%m>0)cnt++;
+					upv=v[bi][j]+cnt;
+				}
+			}
+			else{
+				int cnt=1;
+				while((v[bi][j]+cnt)%b[i][j]>0)cnt++;
+				upv=v[bi][j]+cnt;
+			}
+
+			int lfv=INT_MAX;
+			if(b[i][j]==1){
+				if(b[i][bj]==1)
+					lfv=v[i][bj]+1;
+				else{
+					if(b[i][bj]==0){
+						auto iter=find(rights.begin(),rights.end(),make_pair(i,bj));
+						if(iter!=rights.end()){
+#ifdef debug
+							if(i==6&&j==4)
+								cout<<upv<<' '<<v[i][bj]<<endl;
+
 #endif
-        que.erase(que.begin());
-		if(x==n-1 && y==n-1)
-			continue;
-        for(int i=0;i<4;i++){
-            int dx=dir[i][0],dy=dir[i][1];
-            int nx=x+dx,ny=y+dy;
-            int h;
-            vector<pair<int,int> > tmp;
-            if(i==1 || i==3)
-                tmp=downs;
-            else
-                tmp=rights;
-            
-            if(nx>-1&&nx<n&&ny>-1&&ny<n){
-				if(visited[nx][ny]>0)
-					continue;
-				visited[nx][ny]=1;
-                int bp=b[x][y];
-                int bn=b[nx][ny];
-                if(bn==0){
-                    if(bp==1){
-                        auto iter=find(tmp.begin(),tmp.end(),make_pair(nx,ny));
-                        if(iter!=tmp.end()){
-                            int cnt=0;
-                            while((t+cnt)%m>0)cnt++;
-                            int tmp=t+cnt+1;
-                            if(v[nx][ny]>0)
-                                v[nx][ny]=min(v[nx][ny],tmp);
-                            else
-                                v[nx][ny]=tmp;
-                            
-                            que.push_back(make_pair(nx,ny));
-                        }
-                    }
-                    
-                }
-                else if(bn==1){
-                    int tmp=t+1;
-                    if(v[nx][ny]>0)
-                        v[nx][ny]=min(v[nx][ny],tmp);
-                    else
-                        v[nx][ny]=tmp;
-                    que.push_back(make_pair(nx,ny));
-                }
-                else{
-                    if(bp==1){
-                        int cnt=0;
-                        while((t+cnt)%bn>0)cnt++;
-                        int tmp=t+cnt+1;
-                        if(v[nx][ny]>0)
-                            v[nx][ny]=min(v[nx][ny],tmp);
-                        else
-                        {
-                            v[nx][ny]=tmp;
-                        }
-                        que.push_back(make_pair(nx,ny));
-                    }
-                }
-            }
-        }
-    }
+							lfv=v[i][bj]+1;
+						}
+					}
+					else
+						lfv=v[i][bj]+1;
+				}
+			}
+			else if(b[i][j]==0){
+				auto iter=find(rights.begin(),rights.end(),make_pair(i,j));
+				if(iter!=rights.end()){
+					int cnt=1;
+					while((v[i][bj]+cnt)%m>0)cnt++;
+					lfv=v[i][bj]+cnt;
+				}
+			}
+			else{
+				int cnt=1;
+				while((v[i][bj]+cnt)%b[i][j]>0)cnt++;
+				lfv=v[i][bj]+cnt;
+			}
+			v[i][j]=min(upv,lfv);
+			if(v[i][j]==INT_MAX)
+				v[i][j]=0;
+		}
 }
 int main(){
 #ifdef FILE_CIN
@@ -154,14 +197,13 @@ int main(){
             cout<<endl;
             }
         #endif
-        cout<<v[n-1][n-1]-1<<endl;
+        cout<<v[n-1][n-1]<<endl;
 #ifdef FILE_CIN
     rights.clear();
     downs.clear();
     for(int i=0;i<n;i++)
         for(int j=0;j<n;j++){
             v[i][j]=0;
-            visited[i][j]=0;
 		}
 	}
 #endif
