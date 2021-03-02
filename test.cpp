63502403cb9f1a85e2e7f8c8639a0067e70abe81
이호준 <meister.maestro.b.c@gmail.com>
2021-03-02 09:39:54 +0900

    naver finanacial num 2

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(void){
+    int n=8;
+    int arr[]= {1,1,5,8,9,9,10,11};
+    int x=1;
+    vector<int> A(arr,arr+n);
+    int l=0,r=n-1;
+    while(l<r){
+        int m=(l+r)/2;
+        if(x<A[m]){
+            r=m-1;
+            r==-1?r=0:r=m-1;
+        }
+        else{
+            l=m;
+            l==m?(A[m]==x?r=m:l=m+1):l=m;
+        }
+    }
+    if(A[l]==x)
+    
+        cout<<l<<endl;
+    else
+    {
+        cout<<-1<<endl;
+    }
+    
+    
+}
