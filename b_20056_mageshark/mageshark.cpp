dfed2cd154ab4130f3bb2df0ebedd6b53361b4c6
수행봇048( ) <9000095@mobis.co.kr>
2021-04-08 09:02:06 +0100

    rm CRLF

--- b_20056_mageshark/mageshark.cpp
+++ b_20056_mageshark/mageshark.cpp
@@ -5,8 +5,6 @@
 #define FILE_CIN
 #define elapsedseconds(start) (clock()-start)/(double)CLOCKS_PER_SEC
 using namespace std;
-//ë³´í†µ ?°ì‚°??1?µì— 1ì´ˆì´ì§€ë§?
-//?¨ìˆœ ?˜ì‹ê³„ì‚°, ë©”ëª¨ë¦??‘ê·¼, ?ë£Œêµ¬ì¡°???°ë¼ ?°ì‚°?‰ì? ???˜ì–´?˜ê±°??ì¤„ì–´?????ˆë‹¤.
 int main(){
 #ifdef FILE_CIN
 	freopen(FILENAME,"r",stdin);
